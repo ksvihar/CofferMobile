509fb8d50f4bcf6305e48e465bead9ae583ea875
Vihar <ksvihar@gmail.com>
2014-03-31 18:36:28 +0530

    JS Removed

--- coffermobile.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.move').mouseenter(function(){
-		$(this).css({marginBottom:'10px'});
-	});
-	$('.move').mouseleave(function(){
-		$(this).css({marginBottom:'0px'});	
-	});
-});
